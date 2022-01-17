6eb02aca77b17d57e77db4faf9f820e128decd98
daichi.maruyama <kinonoki01@gmail.com>
2022-01-17 10:46:26 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @micropost = current_user.microposts.build  # 投稿用
-      @pagy, @microposts = pagy(current_user.microposts.order(id: :desc))
+      @pagy, @microposts = pagy(current_user.fees_microposts.order(id: :desc))
     end
   end
 end
